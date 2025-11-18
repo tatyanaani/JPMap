4b57fd3a38bc3f21b857a5f9404521a48bbd52ef
tatyanaani <tatyana.anishchanka@gmail.com>
2025-11-18 13:28:13 +0100

    Initial Commit

---
+++ JPMap/JPMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMapApp.swift
+//  JPMap
+//
+//  Created by Tatyana on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct JPMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
