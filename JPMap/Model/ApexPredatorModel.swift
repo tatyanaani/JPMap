9934831770203affc0409f544cffb7e88508f586
tatyanaani <tatyana.anishchanka@gmail.com>
2025-12-01 18:53:52 +0100

    [feature] added list embedded in NavStack, updated ApexPredatorModel

--- JPMap/Model/ApexPredatorModel.swift
+++ JPMap/Model/ApexPredatorModel.swift
@@ -6,22 +6,44 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct ApexPredatorModel: Decodable {
+struct ApexPredatorModel: Decodable, Identifiable {
     
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum APType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color{
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
     }
 }
 
--- JPMap/Views/ContentView.swift
+++ JPMap/Views/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    let predators = Predators()
+    @State var searchText: String = ""
+    var filteredDinos: [ApexPredatorModel]{
+        if searchText.isEmpty{
+            return predators.apexPredators
+        } else {
+//            return predators.apexPredators.filter({$0.name.lowercased().contains(searchText.lowercased())})
+            return predators.apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
         }
-        .padding()
+    }
+            
+    var body: some View {
+        NavigationStack {
+            List (filteredDinos) {predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack(spacing: 20){
+                        // dino image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white,radius: 2)
+                        
+                        VStack (alignment: .leading){
+                            // dino name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // dino type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(5)
+                            //                        .background(predator.type == "air" ? .green : predator.type == "land" ? .orange :.blue)
+                                .background(predator.type.background)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        }
+                    }
+                }
+
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle(Text("ApexPredators"))
+            .searchable(text: $searchText)
+            .autocorrectionDisabled(true)
+            .animation(.default, value: self.searchText)
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
